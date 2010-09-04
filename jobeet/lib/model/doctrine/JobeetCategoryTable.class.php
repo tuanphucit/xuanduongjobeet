80edf1fc5c0fb6a35a28b90db1b76f1290ee59a7
duongvuxuan <duongvuxuan@c11c4504-daf7-17bf-ec00-9678077e9582>
2010-09-04 07:32:39 +0000

    Day 6 complete :) Too slow :-S !!

--- jobeet/lib/model/doctrine/JobeetCategoryTable.class.php
+++ jobeet/lib/model/doctrine/JobeetCategoryTable.class.php
@@ -1,11 +1,22 @@
 <?php
 
 
-class JobeetCategoryTable extends Doctrine_Table
+/*class JobeetCategoryTable extends Doctrine_Table
 {
     
     public static function getInstance()
     {
         return Doctrine_Core::getTable('JobeetCategory');
     }
+}*/
+class JobeetCategoryTable extends Doctrine_Table
+{
+  public function getWithJobs()
+  {
+    $q = $this->createQuery('c')
+      ->leftJoin('c.JobeetJobs j')
+      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+ 
+    return $q->execute();
+  }
 }
--- jobeet/lib/model/doctrine/JobeetJob.class.php
+++ jobeet/lib/model/doctrine/JobeetJob.class.php
@@ -12,6 +12,10 @@
  */
 class JobeetJob extends BaseJobeetJob
 {
+	public function __toString(){
+		return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+	}
+	
 	public function getCompanySlug()
 	{
 	  return Jobeet::slugify($this->getCompany());
@@ -25,5 +29,14 @@ class JobeetJob extends BaseJobeetJob
 	public function getLocationSlug()
 	{
 	  return Jobeet::slugify($this->getLocation());
+	}
+	
+	public function save(Doctrine_Connection $conn = null){
+		if($this->isNew() && !$this->getExpiresAt()){
+			$now = $this->getCreatedAt() ? $this->getDateTimeObject('created_at')->format('U') : time();
+			$this->setExpiresAt(date('Y-m-d H:i:s', $now + 86400 * sfConfig::get('app_active_days')));
+		}
+		
+		return parent::save($conn);
 	}
 }
