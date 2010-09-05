4ae5f7386a626ad399c9e6e662477b14af4ab3d8
duongvuxuan <duongvuxuan@c11c4504-daf7-17bf-ec00-9678077e9582>
2010-09-05 15:57:47 +0000

    Day 10 complete :D

--- jobeet/lib/model/doctrine/JobeetJobTable.class.php
+++ jobeet/lib/model/doctrine/JobeetJobTable.class.php
@@ -4,6 +4,16 @@
 // lib/model/doctrine/JobeetJobTable.class.php
 class JobeetJobTable extends Doctrine_Table
 {
+  static public $type = array(
+   'full-time' => 'Full time',
+   'part-time' => 'Part time',
+   'freelance' => 'Freelance',
+  );
+  
+  public function getTypes(){
+  	return self::$type;
+  }
+  
   public function retrieveActiveJob(Doctrine_Query $q)
   {
     return $this->addActiveJobsQuery($q)->fetchOne();
@@ -31,6 +41,8 @@ class JobeetJobTable extends Doctrine_Table
  
     $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
       ->addOrderBy($alias . '.created_at DESC');
+      
+    $q->andWhere($alias . '.is_activated = ?', 1);
  
     return $q;
   }
