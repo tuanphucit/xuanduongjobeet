04813fba70c9d42f545d84c8fbfc1614261353c6
duongvuxuan <duongvuxuan@c11c4504-daf7-17bf-ec00-9678077e9582>
2010-09-17 19:23:31 +0000

    Complete day 14

--- trunk/jobeet/lib/model/doctrine/JobeetCategory.class.php
+++ trunk/jobeet/lib/model/doctrine/JobeetCategory.class.php
@@ -11,31 +11,7 @@
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 class JobeetCategory extends BaseJobeetCategory
-{
-	/*public function getActiveJobs($max = 10)
-	{
-	  $q = Doctrine_Query::create()
-	    ->from('JobeetJob j')
-	    ->where('j.category_id = ?', $this->getId())
-	    ->limit($max);
-	 
-	  return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
-	}*/
-	
-	/*public function getSlug()
-	{
-	  return Jobeet::slugify($this->getName());
-	}*/
-	
-	/*public function countActiveJobs()
-	{
-	  $q = Doctrine_Query::create()
-	    ->from('JobeetJob j')
-	    ->where('j.category_id = ?', $this->getId());
-	 
-	  return Doctrine_Core::getTable('JobeetJob')->countActiveJobs($q);
-	}*/
-	
+{	
 	public function getActiveJobsQuery()
 	{
 	  $q = Doctrine_Query::create()
@@ -57,4 +33,10 @@ class JobeetCategory extends BaseJobeetCategory
 	{
 	  return $this->getActiveJobsQuery()->count();
 	}
+	
+	public function getLatestPost()
+    {
+      return $this->getActiveJobs(1)->getFirst();
+    }
+	
 }
