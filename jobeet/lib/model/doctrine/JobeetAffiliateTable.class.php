09baf49e91ccc9ea2e47c423d0e7731c55f624b4
duongvuxuan <duongvuxuan@c11c4504-daf7-17bf-ec00-9678077e9582>
2010-09-18 19:05:50 +0000

    Day 15 complete!

--- jobeet/lib/model/doctrine/JobeetAffiliateTable.class.php
+++ jobeet/lib/model/doctrine/JobeetAffiliateTable.class.php
@@ -3,9 +3,18 @@
 
 class JobeetAffiliateTable extends Doctrine_Table
 {
-    
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('JobeetAffiliate');
-    }
+
+	public static function getInstance()
+	{
+		return Doctrine_Core::getTable('JobeetAffiliate');
+	}
+
+	public function countToBeActivated()
+	{
+		$q = $this->createQuery('a')
+		->where('a.is_active = ?', 0);
+
+		return $q->count();
+	}
+
 }
